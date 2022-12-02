424377d33966940422ffedb7d8906e4a62056ea7
bamidele efunnuga <bamideleefunnuga9@gmal.com>
2022-12-02 17:42:23 +0100

    feat:Create the logger features

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import time
-import pandas as pd
+import tkinter as tk
+import logging
 
-print(time.time())
-pd.DataFrame()
+logger = logging.getLogger()
+
+# logger.debug("This message is important only when debugging the program")
+# logger.info("This message just shows basic information")
+# logger.warning("This message is about something you should pay attention to")
+# logger.error("this message helps to debug an error that occurred in your program")
+
+logger .setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(levelname)s :: %(message)s")
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.mainloop()
+
